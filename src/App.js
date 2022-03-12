ebbbb8af4685e1098e91f182764df4c9d3a54f64
Mamadou <mamadou.sall@42c.fr>
2022-03-12 18:54:26 +0100

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import About from './pages/About'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-
+import Routes from "./components/Routes"
 
 const App = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-        </Routes>    
-    </BrowserRouter>
+    <div>
+        <Routes />
+    </div>
   )
 }
 
