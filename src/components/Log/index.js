3c9cae67e8678254326b373c5c541b48d15f8711
Mamadou <mamadou.sall@42c.fr>
2022-03-13 22:17:15 +0100

    implement login and register

---
+++ src/components/Log/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div className='connection-form'>
+            <div className="form-container">
+                <ul>
+                    <li>S'inscrire</li>
+                    <li>Se connecter</li>
+                </ul>
+                
+            </div>
+        </div>
+    )
+}
+
+export default index
